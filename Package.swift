c54b60bc87c20ff0cfc966094dc03cd6f5c4badb
hungnt1486 <hungnt1486@gmail.com>
2022-10-04 14:09:54 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,8 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FutaBluetooth",
-            dependencies: [],
-            path:"Sources"),
+            dependencies: []),
         .testTarget(
             name: "FutaBluetoothTests",
             dependencies: ["FutaBluetooth"]),

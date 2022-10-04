3388757988916438c653ed1f74b79ba2a7e74622
hungle <hung.le2@futa.vn>
2022-10-04 14:55:06 +0700

    cm

--- FutaBluetooth/Package.swift
+++ FutaBluetooth/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "FutaBluetooth",
-    platforms: [
+    platforms: [    
                 .iOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -24,7 +24,7 @@ let package = Package(
             name: "FutaBluetooth",
             dependencies: [
             ],
-            path:"Sources/FutaBluetooth"),
+            path:"Sources"),
         .testTarget(
             name: "FutaBluetoothTests",
             dependencies: ["FutaBluetooth"]),

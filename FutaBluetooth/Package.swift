f1d97da6a1b8c29b3bd94e01e39bf4d786bccbff
hungle <hung.le2@futa.vn>
2022-10-04 14:12:14 +0700

    cm

--- FutaBluetooth/Package.swift
+++ FutaBluetooth/Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FutaBluetooth",
-            dependencies: []),
+            dependencies: [],
+            path:"Sources/FutaBluetooth/FutaBluetooth"),
         .testTarget(
             name: "FutaBluetoothTests",
             dependencies: ["FutaBluetooth"]),
